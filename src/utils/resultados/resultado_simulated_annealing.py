3b9fcc89ae39846dc7c372ba701e4857206321e1
Víctor Emanuel <victoremanuel2235@gmail.com>
2026-06-08 17:42:52 -0300

    a

--- src/utils/resultados/resultado_simulated_annealing.py
+++ src/utils/resultados/resultado_simulated_annealing.py
@@ -24,14 +24,14 @@ class ResultadoSimulatedAnnealing(Resultado):
             "datasets/resultados_simulated_annealing.csv",
             {
                 "iteracoes": str(self.iteracoes),
+                "ordem_coletaveis": str(self.ordem_coletaveis), # Salvando os coletáveis!
+                "rota_macro": str(self.rota_macro),
+                "curva_convergencia": str(self.curva),
                 "pior_custo": str(self.pior_custo) if self.pior_custo is not None else "",
                 "custo_medio": f"{self.custo_medio:.2f}" if self.custo_medio is not None else "",
                 "tempo_medio": f"{self.tempo_medio:.6f}" if self.tempo_medio is not None else "",
                 "iteracoes_media": f"{self.iteracoes_media:.2f}" if self.iteracoes_media is not None else "",
-                "taxa_sucesso_5porcento": f"{self.taxa_sucesso:.1f}%" if self.taxa_sucesso is not None else "",
-                "ordem_coletaveis": str(self.ordem_coletaveis), # Salvando os coletáveis!
-                "rota_macro": str(self.rota_macro),
-                "curva_convergencia": str(self.curva)
+                "taxa_sucesso_5porcento": f"{self.taxa_sucesso:.1f}%" if self.taxa_sucesso is not None else ""
                 
             }   
         )
