be903fc8cfc7c607b2498633e25db8f1fedc782c
Richardy Tanure <richardy.tanure@gmail.com>
2026-06-07 21:03:24 -0300

    limita algoritmos por conta do consumo de memoria

--- src/utils/run_missing_results.py
+++ src/utils/run_missing_results.py
@@ -22,6 +22,9 @@ def run_missing_results(missing_results, *args, **kwargs):
             continue
 
         for algorithm in missing_algorithms:
+            if(maze.width * maze.length > 100000 and (algorithm == "ONLINE_A*")): continue
+            if(maze.width * maze.length > 250000 and (algorithm == "BFS")): continue
+
             progresso_atual += 1
             porcentagem = (progresso_atual / total_tarefas) * 100
             
